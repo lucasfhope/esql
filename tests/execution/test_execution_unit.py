5ed51014ae5eead229b724fb815377fe689a349b
Lucas Hope <lucasfhope@icloud.com>
2025-04-21 17:36:24 +0200

    ensure order by works for out of order grouping attributes

--- tests/execution/test_execution_unit.py
+++ tests/execution/test_execution_unit.py
@@ -22,7 +22,6 @@ def test_order_by_sort():
         { "cust": "Wally", "prod": "Cherry", "round": 527.54, "sum": 63832 },
         { "cust": "Mia", "prod": "Grapes", "round": 444.25, "sum": 45758 }
     ]
-
     order_by_1 = [ 
         { "cust": "Boo", "prod": "Cherry", "round": 486.66, "sum": 48666 },
         { "cust": "Boo", "prod": "Ice", "round": 472.79, "sum": 45861 },
@@ -39,8 +38,6 @@ def test_order_by_sort():
         { "cust": "Wally", "prod": "Ham", "round": 533.85, "sum": 59257 },
         { "cust": "Wally", "prod": "Cherry", "round": 527.54, "sum": 63832 }
     ]
-        
-
     order_by_2 = [ 
         { "cust": "Boo", "prod": "Cherry", "round": 486.66, "sum": 48666 },
         { "cust": "Boo", "prod": "Ice", "round": 472.79, "sum": 45861 },
@@ -57,19 +54,68 @@ def test_order_by_sort():
         { "cust": "Wally", "prod": "Cherry", "round": 527.54, "sum": 63832 },
         { "cust": "Wally", "prod": "Ham", "round": 533.85, "sum": 59257 }
     ]
-
     assert order_by_sort(
         projected_table=order_by_0,
         order_by=0,
         grouping_attributes=grouping_attributes
     ) == order_by_0
-
     assert order_by_sort(
         projected_table=order_by_0,
         order_by=1,
         grouping_attributes=grouping_attributes
     ) == order_by_1
+    assert order_by_sort(
+        projected_table=order_by_0,
+        order_by=2,
+        grouping_attributes=grouping_attributes
+    ) == order_by_2
 
+
+def test_order_by_sort_works_when_grouping_attributes_are_not_at_the_front():
+    grouping_attributes = ["cust", "prod"]
+    order_by_0 = [ 
+        { "round": 480.41, "cust": "Wally", "sum": 55727, "prod": "Butter" },
+        { "round": 486.66, "cust": "Boo", "sum": 48666, "prod": "Cherry" },
+        { "round": 464.56, "cust": "Helen", "sum": 48779, "prod": "Butter" },
+        { "round": 534.51,"cust": "Wally", "sum": 58262, "prod": "Apple" },
+        { "round": 472.79, "cust": "Boo", "sum": 45861, "prod": "Ice" },
+        { "round": 515.34,  "cust": "Helen", "sum": 60295, "prod": "Ice" },
+        { "round": 533.85, "cust": "Wally", "sum": 59257, "prod": "Ham" },
+        { "round": 493.63, "cust": "Helen",  "sum": 61704, "prod": "Cherry" },
+        { "round": 527.54, "cust": "Wally",  "sum": 63832, "prod": "Cherry" },
+    ]
+    order_by_1 = [
+        { "round": 486.66, "cust": "Boo", "sum": 48666, "prod": "Cherry" },
+        { "round": 472.79, "cust": "Boo", "sum": 45861, "prod": "Ice" },
+        { "round": 464.56, "cust": "Helen", "sum": 48779, "prod": "Butter" },
+        { "round": 515.34,  "cust": "Helen", "sum": 60295, "prod": "Ice" },
+        { "round": 493.63, "cust": "Helen",  "sum": 61704, "prod": "Cherry" },
+        { "round": 480.41, "cust": "Wally", "sum": 55727, "prod": "Butter" },
+        { "round": 534.51,"cust": "Wally", "sum": 58262, "prod": "Apple" },
+        { "round": 533.85, "cust": "Wally", "sum": 59257, "prod": "Ham" },
+        { "round": 527.54, "cust": "Wally",  "sum": 63832, "prod": "Cherry" }
+    ]
+    order_by_2 = [
+        { "round": 486.66, "cust": "Boo", "sum": 48666, "prod": "Cherry" },
+        { "round": 472.79, "cust": "Boo", "sum": 45861, "prod": "Ice" },
+        { "round": 464.56, "cust": "Helen", "sum": 48779, "prod": "Butter" },
+        { "round": 493.63, "cust": "Helen",  "sum": 61704, "prod": "Cherry" },
+        { "round": 515.34,  "cust": "Helen", "sum": 60295, "prod": "Ice" },
+        { "round": 534.51,"cust": "Wally", "sum": 58262, "prod": "Apple" },
+        { "round": 480.41, "cust": "Wally", "sum": 55727, "prod": "Butter" },
+        { "round": 527.54, "cust": "Wally",  "sum": 63832, "prod": "Cherry" },
+        { "round": 533.85, "cust": "Wally", "sum": 59257, "prod": "Ham" }
+    ]
+    assert order_by_sort(
+        projected_table=order_by_0,
+        order_by=0,
+        grouping_attributes=grouping_attributes
+    ) == order_by_0
+    assert order_by_sort(
+        projected_table=order_by_0,
+        order_by=1,
+        grouping_attributes=grouping_attributes
+    ) == order_by_1
     assert order_by_sort(
         projected_table=order_by_0,
         order_by=2,
@@ -83,7 +129,6 @@ def test_projection_rounds_correctly():
     grouping_attributes = ["cust", "prod"]
     column_indices = {"cust": 0, "prod": 1, "quant1": 2, "quant2": 3}
     initial_row = ["Alice", "Apples", 12.514839, 3543.4]
-
     aggregates: AggregatesDict = {
         "global_scope": [
             {"column": "quant1", "function": "sum"},
@@ -93,7 +138,6 @@ def test_projection_rounds_correctly():
         ],
         "group_specific": []
     }
-
     row = GroupedRow(
         grouping_attributes=grouping_attributes,
         aggregates=aggregates,
@@ -101,29 +145,24 @@ def test_projection_rounds_correctly():
         column_indices=column_indices
     )
     table = [row]
-
     parsed_select_clause = ParsedSelectClause(
         grouping_attributes=grouping_attributes,
         aggregates=aggregates,
         select_items_in_order=["cust","prod","quant1.sum","quant1.count", "quant2.sum", "quant2.count"]
     )
-
     expected_result_1 = [ {"cust": "Alice", "prod": "Apples", "quant1.sum": 12.5, "quant1.count": 1, "quant2.sum": 3543.4, "quant2.count": 1} ]
     expected_result_3 = [ {"cust": "Alice", "prod": "Apples", "quant1.sum": 12.515, "quant1.count": 1, "quant2.sum": 3543.400, "quant2.count": 1} ]
     expected_result_5 = [ {"cust": "Alice", "prod": "Apples", "quant1.sum": 12.51484, "quant1.count": 1, "quant2.sum": 3543.40000, "quant2.count": 1} ]
-
     assert project_select_attributes(
         parsed_select_clause=parsed_select_clause,
         grouped_table=table,
         decimal_places=1
     ) == expected_result_1
-
     assert project_select_attributes(
         parsed_select_clause=parsed_select_clause,
         grouped_table=table,
         decimal_places=3
     ) == expected_result_3
-
     assert project_select_attributes(
         parsed_select_clause=parsed_select_clause,
         grouped_table=table,
